3ace7040bcc9f40d8c65e5d11a8c534af52e7477
Hirusha-silva <hirushasilva222@gmail.com>
2025-08-07 14:00:29 +0530

    customer controller

--- src/main/java/com/example/springdeployement/controller/CustomerController.java
+++ src/main/java/com/example/springdeployement/controller/CustomerController.java
@@ -1,11 +1,10 @@
 package com.example.springdeployement.controller;
 
 import com.example.springdeployement.entity.Customer;
+import com.example.springdeployement.service.CustomerService;
 import com.example.springdeployement.service.impl.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,7 +12,25 @@ import java.util.List;
 @RequestMapping("api/v1/customer")
 public class CustomerController {
     @Autowired
-    public CustomerServiceImpl customerServiceImpl;
+    private CustomerService customerService;
 
+    @GetMapping("get")
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
 
+    @PostMapping("create")
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @PutMapping("update")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        return customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping("delete")
+    public void deleteCustomer(@RequestBody Customer customer) {
+        customerService.deleteCustomer(customer);
+    }
 }
